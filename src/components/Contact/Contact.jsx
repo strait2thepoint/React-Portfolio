def97d5922d67bb47b3c650bfa2c2dab17ae8ceb
Strait2thePoint <122751055+strait2thepoint@users.noreply.github.com>
2023-07-02 15:14:43 -0400

    build is working

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h2>Contact Me</h2>
+      <p>
+        You can reach out to me via email or connect with me on GitHub:
+      </p>
+      <ul>
+        <li>Email: sewstrait@gmail.com</li>
+        <li>
+          GitHub: <a href="https://github.com/strait2thepoint">https://github.com/strait2thepoint</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Contact;
