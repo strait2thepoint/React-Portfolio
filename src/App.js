6e9c814ab6921e3e04980d3df3c6659555f4b1d0
Strait2thePoint <122751055+strait2thepoint@users.noreply.github.com>
2023-07-02 21:15:10 -0400

    making progress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import AboutMe from './components/AboutMe/AboutMe';
 import Contact from './components/Contact/Contact';
 import Resume from './components/Resume/Resume';
+import './App.css'
 
-  function App() {
-    return (
-      <div className="portfolio">
+function Footer() {
+  return (
+    <div className="footer">
+      Made with ❤️ by <a href="https://github.com/strait2thepoint">Strait2thePoint</a>. 
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
         <nav>
+          <h2>Laura Strait</h2>
           <ul>
-            <li><a href="#about-me">About Me</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#resume">Resume</a></li>
+            <li>
+              <Link to="/about-me">About Me</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/resume">Resume</Link>
+            </li>
+            
           </ul>
         </nav>
-        <AboutMe id="about-me" />
-        <Contact id="contact" />
-        <Resume id="resume" />
+
+        <Routes>
+          <Route path="/about-me" element={<AboutMe />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
       </div>
-    );
-  }
-  
+
+      <Footer />
+    </Router>
+  );
+}
 
 export default App;
